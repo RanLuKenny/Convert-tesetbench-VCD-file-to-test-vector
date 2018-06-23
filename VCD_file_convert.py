455e82b79cd438f0cff6ffa9bde5239fe71d99ed
Ran Lu <37641171+RanLuKenny@users.noreply.github.com>
2018-06-23 00:23:12 -0500

    Add files via upload

---
+++ VCD_file_convert.py
@@ -0,0 +1,187 @@
+#python file for vcd imformation extraction
+# vcd_file_name = 'test_file.vcd'
+vcd_file_name = 'decoder_tb.vcd'
+
+input_ports = ['clk','din [2:0]',]
+output_ports = ['dout [7:0]']
+
+ports = input_ports + output_ports
+
+ports_adjust = []
+port_and_sign = {}
+muil_but_sign = {}
+
+output_sequence = {}
+
+def handle_mul_bus(port_name, line):
+    if ':' in port_name:
+        if port_name in line:
+            position = line.index(port_name)
+            sign = line[position - 2]
+            muil_but_sign[port_name] = sign
+            # print(muil_but_sign)
+            # muil_but_sign.setdefault(port_name, sign)
+    else:
+        if port_name in line:
+            position = line.index(port_name)
+            sign = line[position - 2]
+            port_and_sign[port_name] = sign
+            # print(port_and_sign)
+            # port_and_sign.setdefault(port_name, sign)
+pass
+
+def array(strings): #convert the bus or port
+    if ':' in strings:
+        pos = strings.index(':')
+        number = int(strings[pos - 1])
+        for i in range(number+1):
+            temp = strings[0: pos - 1] + str(i) + ']'
+            ports_adjust.append(temp)
+    else:
+        ports_adjust.append(strings)
+    #print(ports_adjust)
+pass
+
+def get_wavetime(strings): #timeslot
+    if strings[0] == "#":
+        print(strings[1:])
+pass
+
+def get_ports(port_name, line): # match the sign and port
+    if port_name in line:
+        position = line.index(port_name)
+        sign = line[position-2]
+        port_and_sign[port_name] = sign
+        #print(port_and_sign)
+        return 0
+pass
+
+def ini_sequence():
+    for port in ports:
+        output_sequence[port] = ''
+pass
+
+def store_lines(): #detect #
+    f = open(vcd_file_name)
+    file_lines = []
+    line = f.readline()
+    while line:
+        line = f.readline()
+        remove = line.strip('\t').strip('\n')
+        if remove != '':
+            file_lines.append(remove)
+    f.close()
+    return file_lines
+pass
+
+def detect_signal_value(f):
+    for line in f:
+        sub_line = []
+        if line[0] == '#':
+            for iter in range(f.index(line)+1, len(f) - 1):
+                if f[iter][0] == '#':
+                    break
+                else:
+                    sub_line.append(f[iter])
+            #print(sub_line)
+            #for port in ports_adjust:
+            for port0 in port_and_sign: ### get the value
+                match_signal_value(sub_line, port0)
+            for port1 in muil_but_sign:
+                mul_match_signal_value(sub_line, port1)
+pass
+
+def get_bus_width(string):
+    return int(string[string.index(':')-1])
+pass
+
+def match_signal_value(s_l, port):
+     n = 0
+     #temp = ''
+     for element in s_l:
+        #print(s_l)
+        if(element[1] == port_and_sign[port]):
+            n = 1
+            output_sequence[port] += ' ' + element[0]
+        else:
+            continue
+     if n == 0:
+        S = output_sequence[port]
+        output_sequence[port] += ' ' + S[len(output_sequence[port])-1]
+pass
+
+def mul_match_signal_value(s_l, port):
+    n = 0
+    for element in s_l:
+        if (element[0] == 'b'):
+            if(element[len(element)-1] == muil_but_sign[port]):
+                # print(element[len(element)-1])
+                n = 1
+                output_sequence[port] += ' ' + element[1:len(element)-2]
+        else: continue
+    if n == 0:
+        S = output_sequence[port]
+        temp = S.split(' ')
+        # print(temp)
+        # temp = temp.remove('')
+        # print(S)
+        output_sequence[port] += ' ' + str(temp[len(temp) - 1])#########
+#
+def get_size(string):
+    if ':' in string:
+        size = int(string[string.index(':')-1]) + 1
+        return size
+    else:
+        return 0
+pass
+
+def add_zero(binary, size):
+    while(len(binary) <= size):
+        binary = '0' + binary
+    return binary
+pass
+
+if __name__ == '__main__':
+    for s in ports:
+        array(s)
+
+    ini_sequence()
+
+    f = open(vcd_file_name)
+    lines = f.readline()
+    while lines:
+           lines = f.readline()
+           # for port in port_and_sign :
+           #     get_ports(port, lines) # B[3] B[2] B[1] B[0]
+           for port in ports:
+               # print(port)
+               handle_mul_bus(port, lines)  # A [n:0]
+
+    detect_signal_value(store_lines())
+    # print(output_sequence)
+
+    for key in output_sequence:
+        store = ''
+        value = (output_sequence[key]).split(' ')
+        value.remove('')
+        #print(value)#
+
+        if ':' in key:
+            if key != '':
+                size = abs(int(key[key.index(':') - 1]) - int(key[key.index(':') + 1])) + 1
+                for element in value:
+                    # while len(element) < size:
+                    #     element = '0' + element
+                    if len(element) < size:
+                        element = (size - len(element))*'0' + element
+                    store += ' ' + element
+                        # string.insert(0, '0')
+                output_sequence[key] = store + ' '
+    # print(output_sequence)
+        # output_sequence[key] = store
+
+    wf = open('data.dat', 'w')
+    for key in output_sequence:
+        temp = key + output_sequence[key]
+        print (temp)#
+        wf.write(temp+'\n')
