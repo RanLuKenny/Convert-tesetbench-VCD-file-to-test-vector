c3cf62f23c02b2dc893e091608efe597b55d3953
Ran Lu <37641171+RanLuKenny@users.noreply.github.com>
2018-06-25 11:48:50 -0500

    Update VCD_file_convert.py

--- VCD_file_convert.py
+++ VCD_file_convert.py
@@ -1,9 +1,9 @@
 #python file for vcd imformation extraction
 # vcd_file_name = 'test_file.vcd'
-vcd_file_name = 'decoder_tb.vcd'
+vcd_file_name = 'test_file.vcd'
 
-input_ports = ['clk','din [2:0]']
-output_ports = ['dout [7:0]']
+input_ports = ['CLK','RESET','ENABLE','LOAD']
+output_ports = ['D [3:0]', 'Q [3:0]']
 
 ports = input_ports + output_ports
 
@@ -12,6 +12,7 @@ port_and_sign = {}
 muil_but_sign = {}
 
 output_sequence = {}
+output_sequence_new = {}
 
 def handle_mul_bus(port_name, line):
     if ':' in port_name:
@@ -30,15 +31,15 @@ def handle_mul_bus(port_name, line):
             # port_and_sign.setdefault(port_name, sign)
 pass
 
-def array(strings): #convert the bus or port
+def array(strings, mul_ports): #convert the bus or port
     if ':' in strings:
         pos = strings.index(':')
         number = int(strings[pos - 1])
         for i in range(number+1):
             temp = strings[0: pos - 1] + str(i) + ']'
-            ports_adjust.append(temp)
+            mul_ports.append(temp)
     else:
-        ports_adjust.append(strings)
+        mul_ports.append(strings)
     #print(ports_adjust)
 pass
 
@@ -76,134 +77,4 @@ pass
 
 def detect_signal_value(f):
     for line in f:
-        sub_line = []
-        if line[0] == '#':
-            for iter in range(f.index(line)+1, len(f) - 1):
-                if f[iter][0] == '#':
-                    break
-                else:
-                    sub_line.append(f[iter])
-            #print(sub_line)
-            #for port in ports_adjust:
-            for port0 in port_and_sign: ### get the value
-                match_signal_value(sub_line, port0)
-            for port1 in muil_but_sign:
-                mul_match_signal_value(sub_line, port1)
-pass
-
-def get_bus_width(string):
-    return int(string[string.index(':')-1])
-pass
-
-def match_signal_value(s_l, port):
-     n = 0
-     #temp = ''
-     for element in s_l:
-        #print(s_l)
-        if(element[1] == port_and_sign[port]):
-            n = 1
-            output_sequence[port] += ' ' + element[0]
-        else:
-            continue
-     if n == 0:
-        S = output_sequence[port]
-        output_sequence[port] += ' ' + S[len(output_sequence[port])-1]
-pass
-
-def mul_match_signal_value(s_l, port):
-    n = 0
-    for element in s_l:
-        if (element[0] == 'b'):
-            if(element[len(element)-1] == muil_but_sign[port]):
-                # print(element[len(element)-1])
-                n = 1
-                output_sequence[port] += ' ' + element[1:len(element)-2]
-        else: continue
-    if n == 0:
-        S = output_sequence[port]
-        temp = S.split(' ')
-        # print(temp)
-        # temp = temp.remove('')
-        # print(S)
-        output_sequence[port] += ' ' + str(temp[len(temp) - 1])#########
-pass#
-
-def get_size(string):
-    if ':' in string:
-        size = int(string[string.index(':')-1]) + 1
-        return size
-    else:
-        return 0
-pass
-
-def add_zero(binary, size):
-    while(len(binary) <= size):
-        binary = '0' + binary
-    return binary
-pass
-
-def bin_to_dec(bin):
-    for ele in bin:
-        if ele != '0' and ele !='1':
-            return 'x'
-    temp = int(bin, 2)
-    return str(temp)
-pass
-
-if __name__ == '__main__':
-    for s in ports:
-        array(s)
-
-    ini_sequence()
-
-    f = open(vcd_file_name)
-    lines = f.readline()
-    while lines:
-           lines = f.readline()
-           # for port in port_and_sign :
-           #     get_ports(port, lines) # B[3] B[2] B[1] B[0]
-           for port in ports:
-               # print(port)
-               handle_mul_bus(port, lines)  # A [n:0]
-
-    detect_signal_value(store_lines())
-    # print(output_sequence)
-
-    for key in output_sequence:
-        store = ''
-        value = (output_sequence[key]).split(' ')
-        value.remove('')
-        #print(value)#
-
-        if ':' in key:
-            if key != '':
-                size = abs(int(key[key.index(':') - 1]) - int(key[key.index(':') + 1])) + 1
-                for element in value:
-                    # while len(element) < size:
-                    #     element = '0' + element
-                    if len(element) < size:
-                        element = (size - len(element))*'0' + element
-                    store += ' ' + element
-                        # string.insert(0, '0')
-                output_sequence[key] = store + ' '
-
-    for key in output_sequence:
-        value = output_sequence[key].split(' ')
-        # print(value)
-        # value.remove('')
-        # print(value)
-        empty_string = ''
-        for ele in value:
-            if ele != '':
-                empty_string += bin_to_dec(ele) + ' '
-                print(empty_string)
-        output_sequence[key] = ' ' + empty_string
-        print(empty_string)
-    # print(output_sequence)
-        # output_sequence[key] = store
-    wf = open('data.dat', 'w')
-
-    for key in output_sequence:
-        temp = key + output_sequence[key]
-        print(temp)
-        wf.write(temp+'\n')
+        sub_line
